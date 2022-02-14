e8178ad4b2ae4b488721ae682a73327918c61782
Carlos Torrento <cltorrento@gmail.com>
2022-02-15 00:34:25 +0100

    DBConf ok

--- src/main/java/com/actividades/java/controllers/UsuarioController.java
+++ src/main/java/com/actividades/java/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.actividades.java.controllers;
 
+import com.actividades.java.dao.UsuarioDao;
 import com.actividades.java.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,9 @@ import java.util.List;
 
 @RestController
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
 
     @RequestMapping(value = "usuario/{id}")
     public Usuario getUsuario(@PathVariable String id){
@@ -26,38 +31,7 @@ public class UsuarioController {
 
     @RequestMapping(value = "usuarios")
     public List<Usuario> getUsuarios(){
-
-        List<Usuario> usuarios = new ArrayList<>();
-
-        Usuario usuario = new Usuario();
-        usuario.setId(1L);
-        usuario.setNombre("Carlos");
-        usuario.setApellidos("Torrento");
-        usuario.setEmail("cl@gmail.com");
-        usuario.setTelefono("12345");
-        usuario.setPassword("del1al5");
-
-        Usuario usuario1 = new Usuario();
-        usuario1.setId(1L);
-        usuario1.setNombre("Luis");
-        usuario1.setApellidos("Torrento");
-        usuario1.setEmail("cl@gmail.com");
-        usuario1.setTelefono("12345");
-        usuario1.setPassword("del1al5");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(1L);
-        usuario2.setNombre("Pepe");
-        usuario2.setApellidos("Torrento");
-        usuario2.setEmail("cl@gmail.com");
-        usuario2.setTelefono("12345");
-        usuario2.setPassword("del1al5");
-
-        usuarios.add(usuario1);
-        usuarios.add(usuario2);
-        usuarios.add(usuario);
-
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 
 
